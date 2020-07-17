c6129e9b2619d7bd7ec380d36d1f44c29314b6b6
DESKTOP-CHSEI3M\Administrator <kiwilss@163.com>
2020-07-17 22:01:23 +0800

    recycler

---
+++ app/src/main/java/com/kiwilss/xview/ui/recyclerview/RecyclerViewActivity.kt
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C), 2017-2020, XXX有限公司
+ * FileName: RecyclerViewActivity
+ * Author:   Administrator
+ * Date:     2020/7/17 21:48
+ * Description: {DESCRIPTION}
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.kiwilss.xview.ui.recyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *@FileName: RecyclerViewActivity
+ *@author : Lss Administrator
+ * @e-mail : kiwilss@163.com
+ * @time   : 2020/7/17
+ * @desc   : {DESCRIPTION}
+ */
+class RecyclerViewActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
