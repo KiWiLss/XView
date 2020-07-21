36d8a28d257cd8cc29d4b80a0b6f84622046a6d3
kiwilss <kiwilss@163.com>
2020-07-21 08:07:59 +0800

    dateutils update

--- app/src/main/java/com/kiwilss/xview/utils/date/LocalDateUtil.java
+++ app/src/main/java/com/kiwilss/xview/utils/date/LocalDateUtil.java
@@ -1,16 +1,16 @@
 package com.kiwilss.xview.utils.date;
 
-import android.annotation.SuppressLint;
 import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalUnit;
 import java.util.Date;
 
 /**
@@ -27,72 +27,12 @@ public class LocalDateUtil {
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static void main(String[] args) {
         System.out.println("------------------LocalDateTime--------------");
+        System.out.println(date2String(getNowDateYMD()));
+        System.out.println(date2String(getNowDateYMD(), "yyyy年MM月dd日"));
 
+        System.out.println(string2LocalDate("2020-07-11"));
+        System.out.println(string2LocalDate("2020/07/11", "yyyy/MM/dd"));
 
-
-//        long dayDiff = dayDiff("2020-07-16", "2020-07-20", PATTEN_YMD);
-//        System.out.println(dayDiff);
-//        System.out.println(getNowString());
-//        System.out.println(string2String("2020-07-20 14:34:20"));
-    }
-
-    public static String string2String(String dateTime) {
-        Date date = string2Date(dateTime, "yyyy-MM-dd HH:mm:ss");
-        return date2String(date, "yyyy.MM.dd HH:mm:ss");
-    }
-
-    public static String date2String(Date date, String pattern) {
-        return getDateParser(pattern).format(date);
-    }
-
-    public static Date string2Date(String time, String pattern) {
-        try {
-            return getDateParser(pattern).parse(time);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @SuppressLint("SimpleDateFormat")
-    private static SimpleDateFormat getDateParser(String pattern) {
-        return new SimpleDateFormat(pattern);
-    }
-
-
-    /**
-     * 获取当前时间,返回格式2020-07-20
-     *
-     * @return
-     */
-    public static String getNowString() {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        return simpleDateFormat.format(getNowMills());
-    }
-
-    /**
-     * 获取当前时间的时间戳,毫秒
-     *
-     * @return
-     */
-    public static long getNowMills() {
-        return System.currentTimeMillis();
-    }
-
-    public static long dayDiff(String startTimeStr, String endTimeStr, String format) {
-//        if (TextUtils.isEmpty(startTimeStr) || TextUtils.isEmpty(endTimeStr)) {
-//            return 0;
-//        }
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat formater = new SimpleDateFormat(format);
-        long diff = 0L;
-        try {
-            long startTime = formater.parse(startTimeStr).getTime();
-            long endTime = formater.parse(endTimeStr).getTime();
-            diff = (endTime - startTime) / (1000 * 60 * 60 * 24);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return diff;
     }
 
     /**
@@ -199,6 +139,11 @@ public class LocalDateUtil {
      * 获取当前日期的星期,星期一返回1
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
+    public static int getNowWeek() {
+        return LocalDateTime.now().getDayOfWeek().getValue();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public static int getNowWeek2() {
         DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
         return dayOfWeek.getValue();
@@ -207,6 +152,11 @@ public class LocalDateUtil {
     /**
      * 获取今天是今年的第多少天,示例202
      */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static int getNowDayOfYear() {
+        return LocalDateTime.now().getDayOfYear();
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static int getNowDayOfYear2() {
         return LocalDate.now().getDayOfYear();
@@ -222,7 +172,8 @@ public class LocalDateUtil {
 
 
     /**
-     * 获取指定日期相关,获取指定年月日时分秒的date
+     * 获取指定日期相关
+     * 获取指定年月日时分秒的date,传值示例 2020, 5, 4 , 4, 3, 9示例2020-05-04T04:03:09
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static LocalDateTime getAppointDateYMDHMS(int year, int month, int day, int hour, int minute, int second) {
@@ -231,7 +182,7 @@ public class LocalDateUtil {
     }
 
     /**
-     * 获取指定年月日的date
+     * 获取指定年月日的date,2020-10-04
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static LocalDate getAppointDateYMD(int year, int month, int day) {
@@ -239,6 +190,9 @@ public class LocalDateUtil {
         return localDate;
     }
 
+    /**
+     * 获取指定时分秒的 date,传值示例 4 , 4, 50 返回示例04:04:50
+     */
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static LocalTime getAppointDateTime(int hour, int minute, int second) {
         return LocalTime.of(hour, minute, second);
@@ -259,21 +213,168 @@ public class LocalDateUtil {
     public static int getAppointMonth(LocalDateTime dateTime) {//
         return dateTime.getMonthValue();
     }
+
     /**
      * 获取指定日期的天,返回示例8
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static int getAppointDay(LocalDateTime dateTime){
+    public static int getAppointDay(LocalDateTime dateTime) {
         return dateTime.getDayOfMonth();
     }
+
     /**
      * 获取指定日期的星期,返回示例8
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static int getAppointWeek(LocalDateTime dateTime){
+    public static int getAppointWeek(LocalDateTime dateTime) {
         return dateTime.getDayOfWeek().getValue();
     }
 
+    /**
+     * 获取当前开始时间,零点,返回示例2020-07-20T00:00
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime getNowStart() {
+        return LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+    }
+
+    /**
+     * 获取当前结束时间
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime getNowEnd() {
+        return LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
+    }
+
+    /**
+     * 获取指定时间的开始时间
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime getAppointStart(LocalDate localDateTime) {
+        return LocalDateTime.of(localDateTime, LocalTime.MIN);
+    }
+
+    /**
+     * 获取指定时间的结束时间
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime getAppointEnd(LocalDate localDate) {
+        return LocalDateTime.of(localDate, LocalTime.MAX);
+    }
+
+    /**
+     * 转化相关
+     * LocalDateTime -> string ,默认格式,返回示例2020-07-20 21:44:09和传入的 pattern 有关
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String date2String(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    /**
+     * LocalDateTime -> string ,指定格式,返回和传入的 pattern 有关
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String date2String(LocalDateTime localDateTime, String pattern) {
+        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * LocalDate -->  string ,返回默认格式
+     * System.out.println(date2String(getNowDateYMD())),返回2020-07-20
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String date2String(LocalDate localDate) {
+        return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    /**
+     * LocalDate -->  string,返回指定模式
+     * System.out.println(date2String(getNowDateYMD(),"yyyy年MM月dd日"));
+     * 返回示例:2020年07月20日
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String date2String(LocalDate localDate, String pattern) {
+        return localDate.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * string -->  localdate, string默认格式yyyy-MM-dd,传值2020-07-11返回示例2020-07-11
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDate string2LocalDate(String date) {
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDate.parse(date, pattern);
+    }
+
+    /**
+     * string -->  localdate, string指定格式
+     * 传值2020/07/11,pattern = yyyy/MM/dd
+     * 返回示例2020-07-11
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDate string2LocalDate(String date, String pattern) {
+        DateTimeFormatter patterns = DateTimeFormatter.ofPattern(pattern);
+        return LocalDate.parse(date, patterns);
+    }
+
+    /**
+     * string 类型时间 --> localdatetime  , 传入的值要和 pattern 匹配
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime string2Date(String date) {
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.parse(date, pattern);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime string2Date(String date, String pattern) {
+        DateTimeFormatter patterns = DateTimeFormatter.ofPattern(pattern);
+        return LocalDateTime.parse(date, patterns);
+    }
+
+    /**
+     * date  --->  localdatetime
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime localDate2LocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+    /**
+     * localdatetime  ------>     date
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static Date localDate2Date(LocalDateTime date) {
+        return Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * date    ----->    milli   日期转毫秒
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static Long date2Milli(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    /**
+     * date    ----->    second   日期转秒
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static Long date2Second(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime plus(LocalDateTime time, long number, TemporalUnit field) {
+        return time.plus(number, field);
+    }
+
+    //日期减去一个数,根据field不同减不同值,field参数为ChronoUnit.*
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDateTime minu(LocalDateTime time, long number, TemporalUnit field) {
+        return time.minus(number, field);
+    }
 
 
 }
