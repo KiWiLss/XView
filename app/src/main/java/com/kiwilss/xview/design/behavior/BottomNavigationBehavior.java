450427cbc4a8eadaf7a920dc90553d01481a4e47
kiwilss <kiwilss@163.com>
2020-08-16 23:51:54 +0800

    8-16 refresh---anim

---
+++ app/src/main/java/com/kiwilss/xview/design/behavior/BottomNavigationBehavior.java
@@ -0,0 +1,50 @@
+package com.kiwilss.xview.design.behavior;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+
+/**
+ * Created by chenxz on 2017/11/25.
+ * 下滑一起下滑,下滑到屏幕外就不可见了,上滑一起上滑
+ */
+
+public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<View> {
+    private ObjectAnimator outAnimator, inAnimator;
+
+    public BottomNavigationBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    // 垂直滑动
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
+        if (dy > 0) {// 上滑隐藏
+            if (outAnimator == null) {
+                outAnimator = ObjectAnimator.ofFloat(child, "translationY", 0, child.getHeight() * 2);
+                outAnimator.setDuration(200);
+            }
+            if (!outAnimator.isRunning() && child.getTranslationY() <= 0) {
+                outAnimator.start();
+            }
+        } else if (dy < 0) {// 下滑显示
+            if (inAnimator == null) {
+                inAnimator = ObjectAnimator.ofFloat(child, "translationY", child.getHeight() * 2, 0);
+                inAnimator.setDuration(200);
+            }
+            if (!inAnimator.isRunning() && child.getTranslationY() >= child.getHeight()) {
+                inAnimator.start();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/kiwilss/xview/design/behavior/ScrollAwareFABBehavior.kt
@@ -0,0 +1,78 @@
+package com.kiwilss.xview.design.behavior
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewPropertyAnimatorListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.kiwilss.xview.utils.anim.AnimatorUtil
+
+
+/**
+ * Created by chenxz on 2018/5/13.
+ *
+ * FAB 上滑隐藏 下滑显示
+ */
+class ScrollAwareFABBehavior : FloatingActionButton.Behavior {
+
+    /**
+     * 是否正在动画
+     */
+    private var isAnimateIng = false
+
+    /**
+     * 是否已经显示
+     */
+    private var isShow = true
+
+    constructor(context: Context, attrs: AttributeSet) : super()
+
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout,
+                                     child: FloatingActionButton,
+                                     directTargetChild: View,
+                                     target: View,
+                                     axes: Int,
+                                     type: Int): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type)
+    }
+
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type)
+        // 手指上滑，隐藏FAB
+        if ((dyConsumed > 0 || dyUnconsumed > 0) && !isAnimateIng && isShow) {
+            AnimatorUtil.translateHide(child, object : StateListener() {
+                override fun onAnimationStart(view: View) {
+                    super.onAnimationStart(view)
+                    isShow = false
+                }
+            })
+        } else if (dyConsumed < 0 || dyUnconsumed < 0 && !isAnimateIng && !isShow) {
+            // 手指下滑，显示FAB
+            AnimatorUtil.translateShow(child, object : StateListener() {
+                override fun onAnimationStart(view: View) {
+                    super.onAnimationStart(view)
+                    isShow = true
+                }
+            })
+        }
+    }
+
+
+    internal open inner class StateListener : ViewPropertyAnimatorListener {
+        override fun onAnimationStart(view: View) {
+            isAnimateIng = true
+        }
+
+        override fun onAnimationEnd(view: View) {
+            isAnimateIng = false
+        }
+
+        override fun onAnimationCancel(view: View) {
+            isAnimateIng = false
+        }
+    }
+
+}
