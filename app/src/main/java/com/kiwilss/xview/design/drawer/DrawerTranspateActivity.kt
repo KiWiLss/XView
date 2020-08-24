6772fcc2b4b6bb85fcfe858a1810cfa934adf16a
kiwilss <kiwilss@163.com>
2020-08-24 23:53:19 +0800

    8-24 drawer ++

--- app/src/main/java/com/kiwilss/xview/design/drawer/DrawerTranspateActivity.kt
+++ app/src/main/java/com/kiwilss/xview/design/drawer/DrawerTranspateActivity.kt
@@ -26,40 +26,47 @@ class DrawerTranspateActivity: AppCompatActivity(R.layout.activity_drawer_transp
         super.onCreate(savedInstanceState)
 
         immersionBar {
-            transparentStatusBar()
+            fullScreen(true)
+            //transparentStatusBar()
            //statusBarColor(R.color.blue_74D3FF)
+            statusBarDarkFont(false)
         }
 
 
+        tb_drawer_transpate_toolbar.title = "抽屉透明状态栏"
         setSupportActionBar(tb_drawer_transpate_toolbar)
+
+//        tb_drawer_transpate_toolbar.setNavigationIcon(R.mipmap.ic_navigation_menu)
+//        tb_drawer_transpate_toolbar.setNavigationOnClickListener {
+//            dl_drawer_bottom_drawer.open()
+//        }
 
         //创建返回键，并实现打开关/闭监听
         val actionBarDrawerToggle = ActionBarDrawerToggle(
             this,dl_drawer_transpate_drawer,tb_drawer_transpate_toolbar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-
-        actionBarDrawerToggle.syncState()
         dl_drawer_transpate_drawer.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
 
         //抽屉打开关闭监听
-        dl_drawer_transpate_drawer.addDrawerListener(object : DrawerLayout.DrawerListener{
-            override fun onDrawerStateChanged(newState: Int) {
-
-            }
-
-            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-
-            }
-
-            override fun onDrawerClosed(drawerView: View) {
-
-            }
-
-            override fun onDrawerOpened(drawerView: View) {
-
-            }
-
-        })
+//        dl_drawer_transpate_drawer.addDrawerListener(object : DrawerLayout.DrawerListener{
+//            override fun onDrawerStateChanged(newState: Int) {
+//
+//            }
+//
+//            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+//
+//            }
+//
+//            override fun onDrawerClosed(drawerView: View) {
+//
+//            }
+//
+//            override fun onDrawerOpened(drawerView: View) {
+//
+//            }
+//
+//        })
 
         //侧滑抽屉点击监听
         nv_drawer_transpate_navigation.run {
