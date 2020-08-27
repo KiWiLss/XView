92e9e83e11c01cb7ce0e18084d8add995cc0fa24
DESKTOP-CHSEI3M\Administrator <kiwilss@163.com>
2020-08-27 18:02:32 +0800

    8-27 anim

---
+++ app/src/main/java/com/kiwilss/xview/ui/anim/AnimActivity.kt
@@ -0,0 +1,21 @@
+package com.kiwilss.xview.ui.anim
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kiwilss.xview.R
+
+/**
+ *@FileName: AnimActivity
+ *@author : Lss Administrator
+ * @e-mail : kiwilss@163.com
+ * @time   : 2020/8/27
+ * @desc   : {DESCRIPTION}
+ */
+class AnimActivity: AppCompatActivity(R.layout.activity_anim) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
