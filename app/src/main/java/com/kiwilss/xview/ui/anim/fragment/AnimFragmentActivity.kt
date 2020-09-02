ae1db507cd5e73d06277b5d913e1930c53292ffd
DESKTOP-CHSEI3M\Administrator <kiwilss@163.com>
2020-09-02 19:57:01 +0800

    9-2 fragmentanim

---
+++ app/src/main/java/com/kiwilss/xview/ui/anim/fragment/AnimFragmentActivity.kt
@@ -0,0 +1,29 @@
+package com.kiwilss.xview.ui.anim.fragment
+
+import com.kiwilss.xview.R
+import com.kiwilss.xview.base.BaseActivity
+
+/**
+ *@FileName: AnimFragmentActivity
+ *@author : Lss Administrator
+ * @e-mail : kiwilss@163.com
+ * @time   : 2020/9/2
+ * @desc   : {DESCRIPTION}
+ */
+class AnimFragmentActivity: BaseActivity(R.layout.activity_anim_fragment) {
+    override fun initData() {
+
+    }
+
+    override fun initEvent() {
+
+    }
+
+    override fun initInterface() {
+
+    }
+
+    override fun onReload() {
+
+    }
+}
