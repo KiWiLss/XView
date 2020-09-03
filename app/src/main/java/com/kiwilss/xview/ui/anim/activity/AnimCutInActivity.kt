23cb5faf12e18f016f5426a6360c3d6d5ff57106
DESKTOP-CHSEI3M\Administrator <kiwilss@163.com>
2020-09-03 18:13:02 +0800

    9-3 activity_anim_cutin.xml

---
+++ app/src/main/java/com/kiwilss/xview/ui/anim/activity/AnimCutInActivity.kt
@@ -0,0 +1,25 @@
+package com.kiwilss.xview.ui.anim.activity
+
+import com.kiwilss.xview.R
+import com.kiwilss.xview.base.BaseActivity
+
+/**
+ *@FileName: AnimCutInActivity
+ *@author : Lss Administrator
+ * @e-mail : kiwilss@163.com
+ * @time   : 2020/9/3
+ * @desc   : {DESCRIPTION}
+ */
+class AnimCutInActivity: BaseActivity(R.layout.activity_anim_cutin) {
+    override fun initData() {
+    }
+
+    override fun initEvent() {
+    }
+
+    override fun initInterface() {
+    }
+
+    override fun onReload() {
+    }
+}
