c3eba394276dbd76bb440bf7602c002afb351313
DESKTOP-CHSEI3M\Administrator <kiwilss@163.com>
2020-10-24 18:14:31 +0800

    10-24

---
+++ app/src/main/java/com/kiwilss/xview/ui/imitation/JingDongSearchActivity.kt
@@ -0,0 +1,22 @@
+package com.kiwilss.xview.ui.imitation
+
+import com.kiwilss.xview.base.BaseActivity
+
+/**
+ *@FileName: JingDongSearchActivity
+ *@author : Lss Administrator
+ * @e-mail : kiwilss@163.com
+ * @time   : 2020/10/24
+ * @desc   : {DESCRIPTION}
+ */
+class JingDongSearchActivity: BaseActivity() {
+    override fun initData() {
+
+    }
+
+    override fun initEvent() {
+    }
+
+    override fun initInterface() {
+    }
+}
