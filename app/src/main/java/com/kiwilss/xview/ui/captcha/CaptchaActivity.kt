eba24f66c698926d38eadb708e99124fafba7a7a
DESKTOP-CHSEI3M\Administrator <kiwilss@163.com>
2020-12-16 11:33:54 +0800

    12-16 captcha

---
+++ app/src/main/java/com/kiwilss/xview/ui/captcha/CaptchaActivity.kt
@@ -0,0 +1,28 @@
+package com.kiwilss.xview.ui.captcha
+
+import com.kiwilss.xview.R
+import com.kiwilss.xview.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_captcha.*
+
+/**
+ *@FileName: CaptchaActivity
+ *@author : Lss Administrator
+ * @e-mail : kiwilss@163.com
+ * @time   : 2020/12/16
+ * @desc   : {DESCRIPTION}
+ */
+class CaptchaActivity : BaseActivity(R.layout.activity_captcha){
+    override fun initData() {
+
+    }
+
+    override fun initEvent() {
+        btn_captcha_puzzle.setOnClickListener {
+
+        }
+    }
+
+    override fun initInterface() {
+    }
+
+}
